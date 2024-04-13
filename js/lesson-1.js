236ae7e3d4bab57ec8346924b030bc2e067c2567
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2024-04-13 18:55:47 +0300

    added lesson-1

---
+++ js/lesson-1.js
@@ -0,0 +1,37 @@
+//Даний рядок, що складається із символів, наприклад, 'abcde'.
+// Перевірте, що першим символом цього рядка є буква 'a'.
+// Якщо це так - виведіть 'так', інакше виведіть 'ні'.
+
+// const str = "bcde";
+
+// if ("a" === str[0]) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+// str.startsWith("a") ? console.log("Yes") : console.log("No");
+
+//TODO: 2 ===================================
+// Якщо ваше ім’я містить більше 4 букв і прізвище більше 5, то виведіть на екран число, яке дорівнює сумі букв імені і прізвища.
+// Якщо умова не виконується - вивести “неможливо виконати умову”.
+// Наприклад: “Віктор Сташко” Віктор - 6 літер, Сташко - 6 літер. Виводимо 12.
+// Використовувати вбудований метод length.
+
+// const firstName = "Віктор";
+// const lastName = "Сташко";
+
+// const firstNameLength = firstName.length;
+// const lastNameLength = lastName.length;
+
+// const totalLetters = firstNameLength + lastNameLength;
+
+// if (firstNameLength > 4 && lastNameLength > 5) {
+//   console.log(totalLetters);
+// } else {
+//   console.log("неможливо виконати умову");
+// }
+
+// firstNameLength > 4 && lastNameLength > 5
+//   ? console.log(totalLetters)
+//   : console.log("неможливо виконати умову");
