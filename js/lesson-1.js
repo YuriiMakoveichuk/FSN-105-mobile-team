b451bf7c765f3119ce63a05548b301175eb6c647
Dmytro <dima50066@gmail.com>
2024-04-13 19:28:42 +0200

    work with for

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -68,26 +68,84 @@
 // та залежно від виду напою оголоси суму досплати.Використовувати switch.
 
 //const drink = prompt("choose a drink");
-function orderDrink(drink) {
-  let price = 0;
-  switch (drink) {
-    case "tea":
-      price = 40;
-      break;
+// function orderDrink(drink) {
+//   let price = 0;
+//   switch (drink) {
+//     case "tea":
+//       price = 40;
+//       break;
 
-    case "coffe":
-      price = 60;
-      break;
+//     case "coffe":
+//       price = 60;
+//       break;
 
-    case "juce":
-      price = 50;
-      break;
+//     case "juce":
+//       price = 50;
+//       break;
 
-    default:
-      return "Drink is not exist";
-  }
-  return `Your order ${price} UAH`;
+//     default:
+//       return "Drink is not exist";
+//   }
+//   return `Your order ${price} UAH`;
+// }
+
+// const result = orderDrink(drink);
+// alert(result);
+
+// let count = 0;
+
+// while (count <= 100) {
+//   console.log(count);
+//   count += 1;
+// }
+
+// TODO: 5 ===================================
+// Напишіть цикл, який пропонує ввести
+// число більше 100 через prompt.
+// Якщо відвідувач ввів інше число - попросити
+// ввести ще раз і так далі.
+// Цикл має питати число, поки відвідувач не
+// введе число більше 100, або натисне кнопку
+// скасування в prompt
+
+// let num = Number(prompt("Введіть число більше 100"));
+
+// while (num < 100) {
+//   num = Number(prompt("Введіть число більше 100"));
+// }
+
+//Напишіть цикл, який виводить у консоль
+// числа від max до min за спаданням
+// Додайте усі парні числа від min до max
+
+// const min = 0;
+// const max = 100;
+// let total = 0;
+// for (let i = max; i >= min; i--) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   total += i;
+// }
+// console.log(total);
+
+// Знайдіть число 49  у проміжку чисел від 1 до 100 та вийдіть з циклу.
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i === 49) {
+//     console.log(i);
+//     break;
+//   }
+// }
+
+//  Виведіть методом console.log() зірочки від 1 до 7 штук у вигляді трикутника таким чино
+// *
+// **
+// ***
+// ****
+// *****
+let star = "*";
+for (let i = 0; i < 7; i++) {
+  console.log(star);
+  star += "*";
 }
-
-const result = orderDrink(drink);
-alert(result);
