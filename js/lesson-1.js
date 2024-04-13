4d56cc86d5e44e22e5ec61d6cc96bd2bd339f590
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-04-13 19:18:05 +0300

    add task

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -35,3 +35,30 @@
 // firstNameLength > 4 && lastNameLength > 5
 //   ? console.log(totalLetters)
 //   : console.log("неможливо виконати умову");
+
+// Написати скрипт, який перевіряє чи введене число через prompt потрапляє в діапазон від 55 до 99 включно.
+// Якщо так, вивести в консоль "Число потрапляє в діапазон".
+// Якщо ні, вивести в консоль "Число не потрапляє в діапазон".
+
+// function checkNumber(number) {
+//     return number >= 55 && number <= 99 ? 'Yes' : 'No';
+// }
+
+// const result = checkNumber(60);
+// console.log(result);
+
+// Є 3 вікові групи : діти (0-16), дорослі(17-60), пенсіонери (61-100).
+// До якої групи відноситься Максим, якщо йому 55 років.Назву групи вивести на екран.
+
+// const ageMaxim = 10;
+// if (ageMaxim >= 0 && ageMaxim <= 16) {
+//     console.log('діти');
+// } else if (ageMaxim >= 17 && ageMaxim <= 60) {
+//     console.log('дорослі');
+// } else if (ageMaxim >= 61 && ageMaxim <= 100) {
+//     console.log('пенсіонери');
+// } else {
+//     console.log('горец)');
+// }
+
+// ageMaxim >= 0 && ageMaxim <= 16 ? console.log('діти') : ageMaxim >= 17 && ageMaxim <= 60 ? console.log('дорослі') : ageMaxim >= 61 && ageMaxim <= 100 ? console.log('пенсіонери') : console.log('горец)');
