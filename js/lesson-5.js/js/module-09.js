90221581d3f345ee9df27033cf3adb0940aef54f
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-06-08 19:07:50 +0300

    add module 5

--- js/lesson-5.js/js/module-09.js
+++ js/lesson-5.js/js/module-09.js
@@ -1,1 +1,31 @@
 // ## 1. Theme switcher
+// localStorage.setItem("test", JSON.stringify([1, 2]));
+// localStorage.setItem("test1", true);
+// console.log(JSON.parse(localStorage.getItem("test")));
+
+// // localStorage.removeItem("test");
+// // localStorage.clear();
+// console.log(localStorage.length);
+import { body, checkBox } from "./refs.js";
+import LS_KEY from "./refs.js";
+checkBox.addEventListener("click", (event) => {
+  if (event.currentTarget.checked) {
+    body.classList.replace("light", "dark");
+    localStorage.setItem(LS_KEY, "dark");
+  } else {
+    body.classList.replace("dark", "light");
+    localStorage.setItem(LS_KEY, "light");
+  }
+});
+
+function renderPage() {
+  const lsData = localStorage.getItem(LS_KEY);
+  if (lsData === "dark") {
+    body.classList.replace("light", "dark");
+    checkBox.checked = true;
+  } else {
+    body.classList.replace("dark", "light");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", renderPage);
---
+++ js/lesson-5.js/js/refs.js
@@ -0,0 +1,4 @@
+export const checkBox = document.querySelector("#theme-switch");
+export const body = document.body;
+const LS_KEY = "switch";
+export default LS_KEY;
